2af8c7eeeb451a1e4b4c0177bf2c64318555a265
Daniel Bartl <630704+danielbartl@users.noreply.github.com>
2026-06-20 20:09:10 +0200

    Replace example application with a streamlined default starter, updated dependencies, and static resources integration

--- wicket-spring-boot-starter-parent/wicket-spring-boot-starter/src/main/java/org/wicketstuff/springboot/starter/DefaultWebApplication.java
+++ wicket-spring-boot-starter-parent/wicket-spring-boot-starter/src/main/java/org/wicketstuff/springboot/starter/DefaultWebApplication.java
@@ -13,4 +13,21 @@ public class DefaultWebApplication extends WebApplication {
 	public Class<? extends Page> getHomePage() {
 		return DefaultHomePage.class;
 	}
+
+	@Override
+	protected void init() {
+		super.init();
+
+		// Mount resources to serve style.css and logo.png from the application root path
+		mountResource("style.css", new org.apache.wicket.request.resource.PackageResourceReference(DefaultHomePage.class, "style.css"));
+		mountResource("logo.png", new org.apache.wicket.request.resource.PackageResourceReference(DefaultHomePage.class, "logo.png"));
+
+		// Ensure the package resource guard allows requests for these resources
+		org.apache.wicket.markup.html.IPackageResourceGuard guard = getResourceSettings().getPackageResourceGuard();
+		if (guard instanceof org.apache.wicket.markup.html.SecurePackageResourceGuard) {
+			org.apache.wicket.markup.html.SecurePackageResourceGuard secureGuard = (org.apache.wicket.markup.html.SecurePackageResourceGuard) guard;
+			secureGuard.addPattern("+*.css");
+			secureGuard.addPattern("+*.png");
+		}
+	}
 }
